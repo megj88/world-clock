6b18df1d709e7b2e090b058bd2381ad808c2358a
Megan <115540207+megj88@users.noreply.github.com>
2023-01-01 17:38:57 +0100

    Add current location

--- src/app.js
+++ src/app.js
@@ -42,6 +42,9 @@ setInterval(function () {
 // Drop down
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
